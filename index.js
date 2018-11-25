28dac3586bf5244cd74ce46e06359a38748e543b
dcatfly <772736731@qq.com>
2018-11-25 18:25:54 +0800

    change from to auto & optimize code

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const isChinese = require('is-chinese');
 
 const q = alfy.input;
 const to = isChinese(q) ? 'en' : 'zh-CN';
-const from = isChinese(q) ? 'zh-CN' : 'en';
+const from = 'auto';
 
 translate(q, {raw: true, to: to}).then(data => {
+  const quicklookurl = `https://translate.google.cn/#${from}/${to}/${encodeURIComponent(q)}`;
   const output = {
     variables: {
       pronounce: 0
-    }, 
+    },
     items: []
+  };
+  const mods = {
+    cmd: {
+      subtitle: '请按 Enter 发音',
+      variables: {
+        pronounce: 1
+      }
+    }
   };
 
   const rawObj = JSON.parse(data.raw);
@@ -21,46 +30,29 @@ translate(q, {raw: true, to: to}).then(data => {
       rawObj[1].forEach(r => {
         const partOfSpeech = r[0];
         r[2].forEach(x => {
-          const text = x[0];
-          const relation = x[1];
-          output.items.push({ 
+          const [text, relation] = x;
+          output.items.push({
+            mods, quicklookurl,
             title: text, 
             subtitle: `(${partOfSpeech}) ${relation.join(', ')}`, 
             arg: text,
-            mods: {
-              cmd: {
-                subtitle: '请按 ⌅ 发音',
-                variables: {
-                  pronounce: 1
-                }
-              }
-            },
-            quicklookurl: `https://translate.google.cn/#${from}/${to}/${encodeURIComponent(q)}`
           });
         });
       });
     } else {
-      output.items.push({ 
+      output.items.push({
+        mods, quicklookurl,
         title: data.text, 
         subtitle: '', 
         arg: data.text,
-        mods: {
-          cmd: {
-            subtitle: '请按 ⌅ 发音',
-            variables: {
-              pronounce: 1
-            }
-          }
-        },
-        quicklookurl: `https://translate.google.cn/#${from}/${to}/${encodeURIComponent(q)}` 
       });
     }
   } else {
     const corrected = data.from.text.value.replace(/\[/, '').replace(/\]/, '');
     output.items.push({ 
       title: data.text, 
-      subtitle: `您要查询的是 ${corrected} 吗?, 请按 ⇥ 查询更多`, 
-      autocomplete: corrected 
+      subtitle: `您要查询的是 ${corrected} 吗?, 请按 Enter 查询更多`,
+      autocomplete: corrected
     });
   }
   
