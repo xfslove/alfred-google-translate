d0c6fdc1b39a8b86c7a185ad1f6b5f4e50e7a064
Ricardo-Ke <ricardo.kefan@gmail.com>
2019-06-20 17:40:49 +0800

    set socks_proxy not turned by default and update README.md

--- agent.js
+++ agent.js
@@ -1,6 +1,12 @@
 var process = require('process')
 var SocksProxyAgent = require('socks-proxy-agent');
-var proxy = process.env.socks_proxy || 'socks://127.0.0.1:1086';
-var agent = new SocksProxyAgent(proxy);
+var proxy = process.env.socks_proxy;
+var agent;
+
+if (!proxy) {
+    agent = undefined
+}else {
+    agent = new SocksProxyAgent(proxy);
+}
 
 module.exports = agent
